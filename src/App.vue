c345f0af32dac4c0161af39abd014b008c705f06
Xavier J <xjulien@octo.com>
2017-02-24 15:46:37 +0100

    Add statics files from mircroglob-static GitHub repository for Messages components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="app">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    font-size: 14px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    background-color: #f5f8fa;
+  }
+
+  *:focus {
+    outline: none;
+  }
+
+  .app {
+    display: flex;
+    justify-content: center;
+    padding: 150px 0;
+  }
+
+  input {
+    font-size: 14px;
+    height: 40px;
+    box-sizing: border-box;
+    border-radius: 3px;
+    color: #666;
+    padding: 4px 14px;
+    border: 1px solid #99CDE1;
+  }
 </style>
---
+++ src/components/Messages/index.vue
@@ -0,0 +1,10 @@
+<template src="./template.html"></template>
+
+<script>
+  export default {
+    name: 'message',
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./style.css"></style>
