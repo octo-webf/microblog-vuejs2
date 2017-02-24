813fad91515bb4c0aad1c010da91187c6fc51e4b
Xavier J <xjulien@octo.com>
2017-02-24 15:35:47 +0100

    Rename Hello.vue to index.vue

--- src/components/Hello/index.vue
+++ src/components/Hello/index.vue
@@ -1,14 +1,14 @@
 <template src="./template.html"></template>
 
 <script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
