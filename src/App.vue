38bda207f8b597a572b6ff84aa849f14d5b6cfc4
Xavier J <xjulien@octo.com>
2017-02-28 11:26:36 +0100

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app" class="app">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Navbar from './components/Navbar';
+
   export default {
     name: 'app',
+
+    components: {
+      Navbar,
+    },
   };
 </script>
 
 <style>
+  @import '../node_modules/font-awesome/css/font-awesome.min.css';
+
   body {
     margin: 0;
     font-size: 14px;
@@ -20,6 +29,10 @@
 
   *:focus {
     outline: none;
+  }
+
+  a {
+    text-decoration: none;
   }
 
   .app {
---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,10 @@
+<template src="./template.html"></template>
+
+<script>
+  export default {
+    name: 'navbar',
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./style.css"></style>
