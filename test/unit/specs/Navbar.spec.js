91af447317e39b240294da61d02d4fc7d159c01c
Xavier J <xjulien@octo.com>
2017-03-03 11:58:33 +0100

    Add tests for navbar

---
+++ test/unit/specs/Navbar.spec.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Navbar from 'components/Navbar';
+
+describe('Navbar components', () => {
+  const Constructor = Vue.extend(Navbar);
+  const vm = new Constructor().$mount();
+
+  it('should render correct contents', () => {
+    expect(vm.$el.querySelector('#navbar #navbar_home .navigation-bar__link-label').textContent)
+      .to.equal('Home');
+
+    expect(vm.$el.querySelector('#navbar #navbar_about .navigation-bar__link-label').textContent)
+      .to.equal('About');
+
+    expect(vm.$el.querySelector('#navbar #navbar_home router-link').getAttribute('to'))
+      .to.equal('/');
+
+    expect(vm.$el.querySelector('#navbar #navbar_about router-link').getAttribute('to'))
+      .to.equal('/about');
+  });
+});
