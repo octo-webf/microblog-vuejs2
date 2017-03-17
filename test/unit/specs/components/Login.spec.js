b73fe4fd8674fe766b7f7af83a6b0294908a63fe
Xavier J <xjulien@octo.com>
2017-03-17 18:25:04 +0100

    Add login test for component Login

--- test/unit/specs/components/Login.spec.js
+++ test/unit/specs/components/Login.spec.js
@@ -1,14 +1,26 @@
 import Vue from 'vue';
+import router from 'src/router';
 import Login from 'components/Login';
+import Auth from 'services/Auth';
 
 describe('components', () => {
   describe('Login', () => {
     const Constructor = Vue.extend(Login);
-    const vm = new Constructor().$mount();
+    const vm = new Constructor({ router }).$mount();
 
     it('should render correct contents', () => {
       expect(vm.$el.querySelector('#login h1').textContent)
         .to.equal('Login');
+    });
+
+    describe('login()', () => {
+      beforeEach(() => {
+        sinon.spy(Auth, 'login');
+      });
+      it('should use Auth service to login the user', () => {
+        vm.login();
+        sinon.assert.calledOnce(Auth.login);
+      });
     });
   });
 });
