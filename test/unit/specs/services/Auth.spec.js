d02650ebbd1204b0958c5943123506cf6a3fb160
Xavier J <xjulien@octo.com>
2017-03-31 15:35:02 +0200

    Add tests for all Auth service functions

--- test/unit/specs/services/Auth.spec.js
+++ test/unit/specs/services/Auth.spec.js
@@ -3,19 +3,74 @@ import Auth from 'services/Auth';
 describe('services', () => {
   describe('Auth', () => {
     describe('pseudo()', () => {
-      describe('when user is logout', () => {
+      context('when user is logout', () => {
         it('should return an empty string', () => {
           expect(Auth.pseudo()).to.equal('');
         });
       });
-      describe('when user is login', () => {
+
+      context('when user is login', () => {
         const pseudo = 'Plop24';
-        beforeEach(() => {
+
+        before(() => {
           sinon.stub(Auth, 'ready').returns(true);
           sinon.stub(localStorage, 'getItem').withArgs('user').returns(JSON.stringify({ name: pseudo }));
         });
+        after(() => {
+          sinon.restore(Auth);
+        });
+
         it('should return \'Pseudo\'', () => {
           expect(Auth.pseudo()).to.equal(pseudo);
+        });
+      });
+    });
+
+    describe('login()', () => {
+      before(() => {
+        sinon.spy(localStorage, 'setItem');
+      });
+
+      after(() => {
+        Auth.logout();
+      });
+
+      it('set user item', () => {
+        Auth.login('test');
+
+        sinon.assert.calledWithExactly(localStorage.setItem, 'user', JSON.stringify({ name: 'test' }));
+      });
+    });
+
+    describe('logout()', () => {
+      before(() => {
+        sinon.spy(localStorage, 'removeItem');
+      });
+
+      it('remove user item', () => {
+        Auth.logout();
+        sinon.assert.calledWithExactly(localStorage.removeItem, 'user');
+      });
+    });
+
+    describe('ready()', () => {
+      context('when user is login', () => {
+        before(() => {
+          Auth.login('test');
+        });
+
+        it('return true', () => {
+          expect(Auth.ready()).to.equal(true);
+        });
+      });
+
+      context('when user is logout', () => {
+        before(() => {
+          Auth.logout();
+        });
+
+        it('return false', () => {
+          expect(Auth.ready()).to.equal(false);
         });
       });
     });
